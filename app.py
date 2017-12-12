fd6fd24be8df39693322ead397ecb09d55476e12
JYlag <joeylagan@gmail.com>
2017-12-11 21:16:10 -0800

    Added pictures

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://portfolio:portfolio@localhost:8889/portfolio')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://website:website@localhost:8889/website')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
